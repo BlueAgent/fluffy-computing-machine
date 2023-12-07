1695a690bf82648ba5c207b0804b9a7357321c76
BlueAgent <BlueAgent@users.noreply.github.com>
2023-12-07 23:43:57 +0800

    Try use two separate loops

--- cc/ic2c_uumcropana_ext.lua
+++ cc/ic2c_uumcropana_ext.lua
@@ -1,20 +1,33 @@
 -- The uu crop library needs to be below the turtle.
 -- Output needs to be in front.
+local CROP_SEED_SLOT = 0 + 1
+local uumCropAna = peripheral.find("ic2:uu_crop_library")
 
 local running = true
-local function loopMain()
+local function loopSuck()
+  local hadSeed = false
   while running do
-    os.sleep(1)
-
-    if turtle.suckDown() then
-      turtle.dropDown()
-      os.sleep(2)
+    os.sleep(2)
+    local hasSeed = uumCropAna.getItemDetail(CROP_SEED_SLOT) ~= nil
+    if hadSeed ~= hasSeed then
+      hadSeed = hasSeed
+    elseif hadSeed and hasSeed then
       turtle.suckDown()
     end
+  end
+end
 
-    local selectedItem = turtle.getItemDetail(turtle.getSelectedSlot(), true)
-    if selectedItem ~= nil and turtle.drop() then
-      print(("Moved %ix %s"):format(selectedItem.count, selectedItem.displayName))
+local function loopDrop()
+  while running do
+    os.sleep(2)
+    for i = 1, 16 do
+      if turtle.getItemCount(i) > 0 then
+        turtle.select(i)
+        local selectedItem = turtle.getItemDetail(turtle.getSelectedSlot(), true)
+        if selectedItem ~= nil and turtle.drop() then
+          print(("Moved %ix %s"):format(selectedItem.count, selectedItem.displayName))
+        end
+      end
     end
   end
 end
@@ -29,4 +42,4 @@ local function loopEvent()
   end
 end
 
-parallel.waitForAll(loopEvent, loopMain)
+parallel.waitForAll(loopEvent, loopSuck, loopDrop)

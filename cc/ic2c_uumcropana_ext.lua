ab62f9857ee491bb8c2bb1eedc2d0e15b260f389
BlueAgent <BlueAgent@users.noreply.github.com>
2023-12-07 23:27:21 +0800

    No more peripheral usage

--- cc/ic2c_uumcropana_ext.lua
+++ cc/ic2c_uumcropana_ext.lua
@@ -1,23 +1,15 @@
 -- The uu crop library needs to be below the turtle.
 -- Output needs to be in front.
-local CROP_SEED_SLOT = 0 + 1
-local uumCropAna = peripheral.find("ic2:uu_crop_library")
 
 local running = true
 local function loopMain()
-  local lastItem = nil
   while running do
-    os.sleep(2)
+    os.sleep(1)
 
-    local currentItem = uumCropAna.getItemDetail(CROP_SEED_SLOT)
-    if currentItem ~= nil and lastItem == currentItem then
-      if turtle.suckDown() then
-        lastItem = nil
-      else
-        lastItem = uumCropAna.getItemDetail(CROP_SEED_SLOT)
-      end
-    else
-      lastItem = currentItem
+    if turtle.suckDown() then
+      turtle.dropDown()
+      os.sleep(2)
+      turtle.suckDown()
     end
 
     local selectedItem = turtle.getItemDetail(turtle.getSelectedSlot(), true)

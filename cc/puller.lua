043e694c33c489a81ceeb2db65e163b23df9cdb9
BlueAgent <BlueAgent@users.noreply.github.com>
2023-12-19 18:15:29 +0800

    Make puller.lua pull instead of push

--- cc/puller.lua
+++ cc/puller.lua
@@ -1,42 +1,42 @@
--- Push items from specific slots
+-- Pull items to specific slots
 
-local SRC_NAME = "top"
-local SRC_SLOTS = {}
 local DST_NAME = "right"
+local DST_SLOTS = {}
+local SRC_NAME = "top"
 
-if not peripheral.hasType(SRC_NAME, "inventory") then
-    print(("Could not find an inventory on the %s side."):format(SRC_NAME))
+if not peripheral.hasType(DST_NAME, "inventory") then
+    print(("Could not find an inventory on the %s side."):format(DST_NAME))
     return
 end
 
-local inv = peripheral.wrap(SRC_NAME)
+local inv = peripheral.wrap(DST_NAME)
 local running = true
 
-local function pushItems(slot)
-  local num = inv.pushItems(DST_NAME, slot)
+local function pullItems(slot)
+  local num = inv.pullItems(SRC_NAME, slot)
   if num > 0 then
-    print(("Moved %ix from slot %i"):format(num, slot))
+    print(("Moved %ix to slot %i"):format(num, slot))
   end
 end
 
 local function loopMain()
-  if #SRC_SLOTS > 0 then
+  if #DST_SLOTS > 0 then
     while running do
-      for _, slot in ipairs(SRC_SLOTS) do
-        pushItems(slot)
+      for _, slot in ipairs(DST_SLOTS) do
+        pullItems(slot)
       end
     end
   else
     while running do
       for slot=1,inv.size() do
-        pushItems(slot)
+        pullItems(slot)
       end
     end
   end
 end
 
 local function loopEvent()
-  print("Item Pusher Running...")
+  print("Item Puller Running...")
   while running do
     local event, p1 = os.pullEventRaw()
     if event == 'terminate' then
